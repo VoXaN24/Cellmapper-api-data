359ccc9e16d3c257526d72868a0018a12f62a573
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2024-08-17 22:46:51 +0000

    Intégration des Proxy Perso

--- script.py
+++ script.py
@@ -22,8 +22,35 @@ def get_mullvad_proxy():
             ip = match.group()
             if ping_ip(ip):
                 ips.append(ip)
-
     return ips
 
-print(get_mullvad_proxy())
+def get_perso_proxy():
+    print(f"Le fichiers doit contenir 1 IP par ligne, et doit être accésible directement via une URL (Vous pouvez utiliser https://litterbox.catbox.moe/ pour heberger temporairement la liste)\n")
+    url=input("URL du fichier\n")
+    response^=requests.get(url)
+    lines=response.text.splitlines()
+    ips=[]
+    for line in lines:
+        if ping_ip(line):
+            ips.append(line)
+    return ips
 
+def menu_choose_proxy():
+    print("|"+"-"*22+"|")
+    print(f"|1 - Proxy MullVad     |")
+    print(f"|2 - Proxy Publique    |")
+    print(f"|3- Proxy Personnalisé |")
+    print("|"+"-"*22+"|")
+
+def choose_proxy():
+    choix=0
+    while choix != "1" and choix != "2" and choix != "3":
+        print(menu_choose_proxy())
+        choix=input("\n")
+    if choix == "1":
+        return get_mullvad_proxy()
+    elif choix == "2":
+        return get_public_proxy() #Non implémentais
+    elif choix == "3":
+        return get_perso_proxy()
+print(choose_proxy())
