a149f22f885e29e4cba81c4d3d3d0f42bf9d4187
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2024-08-20 01:20:27 +0200

    Update DB (Stop at Chile)

--- web/mncmmc.py
+++ web/mncmmc.py
@@ -6,11 +6,7 @@ def add_new():
         mnc=int(input("Enter MNC: "))
         mmc=int(input("Enter MMC: "))
         list_of_networks.append(f'INSERT INTO mmcmnc_cellmapper (country_code, network_name, mnc, mmc) VALUES ("{country_code}", "{name}", {mnc}, {mmc});')
-        if input("Add another network? (y/n): ") == "y":
-            add_new()
-        else:
-            for i in range(len(list_of_networks)):
-                print(list_of_networks[i])
+        add_new()
     except:
         print("Invalid input")
         for i in range(len(list_of_networks)):
