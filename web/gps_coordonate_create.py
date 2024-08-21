ccef9cf12255dadb15968c60deb5f54f379844e9
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2024-08-21 23:35:25 +0200

    Create script for GPS Coordonate (Temporarly to made the data)

---
+++ web/gps_coordonate_create.py
@@ -0,0 +1,12 @@
+list_gps = []
+def create_list_gps_coordonates():
+    try :
+        country = input("Enter the country abv : ")
+        latitude = float(input("Enter the latitude : "))
+        longitude = float(input("Enter the longitude : "))
+        list_gps.append(f'INSERT INTO "main"."gps" ("country", "lat", "long") VALUES ("{country}", {latitude}, {longitude});')
+
+    except ValueError:
+        print("Unvalid value")
+        for i in list_gps:
+            print(i)
