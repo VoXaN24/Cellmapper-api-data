5500876daca6622d1829f9199aa7a362ed0424ae
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2024-10-27 22:32:29 +0100

    a

--- web/gps_coordonate_create.py
+++ web/gps_coordonate_create.py
@@ -5,7 +5,7 @@ def create_list_gps_coordonates():
         latitude = float(input("Enter the latitude : "))
         longitude = float(input("Enter the longitude : "))
         list_gps.append(f'INSERT INTO "main"."gps" ("country", "lat", "long") VALUES ("{country}", {latitude}, {longitude});')
-
+        create_list_gps_coordonates()
     except ValueError:
         print("Unvalid value")
         for i in list_gps:
